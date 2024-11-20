e29a7e108a2a26630b50778a81240fd0d5916bd3
petertasca <tascapeter@gmail.com>
2024-11-20 10:23:27 -0500

    initial commit -- site finished

---
+++ js/main.js
@@ -0,0 +1,82 @@
+document.getElementById('jonSnow').onclick = snowParty
+document.getElementById('aryaStark').onclick = starkParty
+document.getElementById('daenerys').onclick = targParty
+document.getElementById('viserys').onclick = viserysUhOh
+document.getElementById('jamieLannister').onclick = lannParty
+document.getElementById('theHound').onclick = houndParty
+document.getElementById('bronn').onclick = bronnParty
+document.getElementById('brienne').onclick = tarthParty
+document.getElementById('jorah').onclick = jorahParty
+
+
+
+function snowParty() {
+
+    document.querySelector('#pictureHolder').style.backgroundImage = 'url(https://i.gifer.com/1PX8.gif)'
+    document.querySelector('#pictureHolder').classList.add('active')
+
+    document.querySelector('#pictureHolder').style.backgroundSize = 'cover'
+    document.querySelector('#pictureHolder').style.backgroundRepeat = 'no-repeat'
+}
+
+function starkParty() {
+    console.log('stark party')
+    document.querySelector('#pictureHolder').style.backgroundImage = 'url(https://i.gifer.com/M97L.gif)'
+    document.querySelector('#pictureHolder').classList.add('active')
+    document.querySelector('#pictureHolder').style.backgroundSize = 'cover'
+    document.querySelector('#pictureHolder').style.backgroundRepeat = 'no-repeat'
+}
+
+function targParty() {
+    document.querySelector('#pictureHolder').style.backgroundImage = 'url(https://i.gifer.com/66Yo.gif)'
+    document.querySelector('#pictureHolder').classList.add('active')
+    document.querySelector('#pictureHolder').style.backgroundSize = 'cover'
+    document.querySelector('#pictureHolder').style.backgroundRepeat = 'no-repeat'
+}
+
+function lannParty() {
+    document.querySelector('#pictureHolder').style.backgroundImage = 'url(https://i.gifer.com/4z1m.gif)'
+    document.querySelector('#pictureHolder').classList.add('active')
+    document.querySelector('#pictureHolder').style.backgroundSize = 'cover'
+    document.querySelector('#pictureHolder').style.backgroundRepeat = 'no-repeat'
+}
+
+function houndParty() {
+    document.querySelector('#pictureHolder').style.backgroundImage = 'url(https://i.gifer.com/G8z.gif)'
+    document.querySelector('#pictureHolder').classList.add('active')
+    document.querySelector('#pictureHolder').style.backgroundSize = 'cover'
+    document.querySelector('#pictureHolder').style.backgroundRepeat = 'no-repeat'
+
+}
+
+function bronnParty() {
+    document.querySelector('#pictureHolder').style.backgroundImage = 'url(https://i.gifer.com/2Hy3.gif)'
+    document.querySelector('#pictureHolder').classList.add('active')
+    document.querySelector('#pictureHolder').style.backgroundSize = 'cover'
+    document.querySelector('#pictureHolder').style.backgroundRepeat = 'no-repeat'
+}
+
+function tarthParty() {
+    document.querySelector('#pictureHolder').style.backgroundImage = 'url(https://i.gifer.com/99Rz.gif)'
+    document.querySelector('#pictureHolder').classList.add('active')
+    document.querySelector('#pictureHolder').style.backgroundSize = 'cover'
+    document.querySelector('#pictureHolder').style.backgroundRepeat = 'no-repeat'
+
+}
+
+function jorahParty() {
+    document.querySelector('#pictureHolder').style.backgroundImage = 'url(https://i.gifer.com/FzqQ.gif)'
+    document.querySelector('#pictureHolder').classList.add('active')
+    document.querySelector('#pictureHolder').style.backgroundSize = 'cover'
+    document.querySelector('#pictureHolder').style.backgroundRepeat = 'no-repeat'
+
+}
+
+
+function viserysUhOh() {
+    document.querySelector('#pictureHolder').style.backgroundImage = 'url(https://i.gifer.com/DNn9.gif)'
+    document.querySelector('#pictureHolder').classList.add('active')
+    document.querySelector('#pictureHolder').style.backgroundSize = 'cover'
+    document.querySelector('#pictureHolder').style.backgroundRepeat = 'no-repeat'
+}
+
